d93538edd2b5612df6c5f2b4c321458d2f1ba648
Shuhao Wu <shuhao@shuhaowu.com>
2016-07-06 21:29:45 -0400

    Updated to support words

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
 from flask import Flask
 
+from ciphersolver import solve
+
+
 app = Flask(__name__)
 
 
 @app.route("/")
 def main():
   return "Hello World!"
+
+
+@app.route("/words/<w>")
+def words(w):
+  solved, suspected = solve(w)
+
+  return str(suspected)
 
 
 if __name__ == "__main__":
